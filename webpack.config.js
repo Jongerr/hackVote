78792aa04f1e93ef8b10e7f24e9626c01cd697f8
Jongerr <jontmichie@gmail.com>
2017-12-29 17:27:51 -0600

    Add react base for react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/index.jsx',
   output: {
     path: path.resolve('dist'),
     filename: 'bundle.js'   
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   }
