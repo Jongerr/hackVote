1c4170ba655f475d929030190478cfb25f99e20d
Jongerr <jontmichie@gmail.com>
2017-12-31 16:31:48 -0600

    Add test data for topic testing

---
+++ server/helpers/testData.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'someHash1': {
+    topic: 'Where to eat',
+    choices: [
+      'Here',
+      'There',
+      'Everywhere'
+    ]
+  },
+  'someHash2': {
+    topic: 'When to leave',
+    choices: [
+      'Now',
+      'Never',
+      'Whenever',
+      'in 1 hour',
+      'yesterday'
+    ]
+  },
+  'someHash3': {
+    topic: 'Who should drive',
+    choices: [
+      'Me',
+      'You',
+      'Them',
+      'No one'
+    ]
+  }
+}
