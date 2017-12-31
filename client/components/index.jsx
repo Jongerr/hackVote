817e432ff8c798657ccfe9a05f843c346a8a8e0a
Jongerr <jontmichie@gmail.com>
2017-12-31 16:07:25 -0600

    Add component for submitting topics

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Topic from './topic.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <div>
         <span>Hello World!</span>
+        <Topic />
       </div>
     )
   }
---
+++ client/components/topic.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Topic extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      choices: [],
+      currentChoice: '',
+      topic: ''
+    }
+
+    this.handleChoiceChange = this.handleChoiceChange.bind(this);
+    this.handleChoiceSubmit = this.handleChoiceSubmit.bind(this);
+    this.handleTopicChange = this.handleTopicChange.bind(this);
+    this.handleTopicSubmit = this.handleTopicSubmit.bind(this);
+  }
+
+  handleChoiceChange(e) {
+    this.setState({currentChoice: e.target.value});
+  }
+
+  handleChoiceSubmit() {
+    this.state.choices.push(this.state.currentChoice);
+    this.setState({currentChoice: ''});
+    console.log('Topic:', this.state.topic);
+    console.log('Choices:', this.state.choices);
+  }
+
+  handleTopicChange(e) {
+    this.setState({topic: e.target.value});
+  }
+
+  handleTopicSubmit(e) {
+    console.log('Submitting topic', this.state.topic);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="topic-container">
+        <form onSubmit={this.handleTopicSubmit}>
+          Topic: <input type="text" onChange={this.handleTopicChange} value={this.state.topic}/><br/>
+          <input type="text" onChange={this.handleChoiceChange} value={this.state.currentChoice}/>
+          <button type="button" onClick={this.handleChoiceSubmit}>Add choice</button><br/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default Topic;
