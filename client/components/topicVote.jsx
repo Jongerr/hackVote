d273becf42cfa6102d2143505f42d60d8a27bd39
Jongerr <jontmichie@gmail.com>
2017-12-31 20:07:03 -0600

    Add vote toggling

--- client/components/topicVote.jsx
+++ client/components/topicVote.jsx
@@ -29,13 +29,24 @@ class TopicVote extends React.Component {
       this.setState({
         topic: response.topic,
         choices: response.choices
-      })
+      });
     })
     .catch((err) => console.log(err));
   }
 
   handleUpvote(e) {
-    console.log('Upvoted choice at index:', e.target.value);
+    console.log('Upvoted choice at index:', e.target.classList[0]);
+    let newChoices = this.state.choices.slice();
+    if(e.target.classList[0] === 'inactive') {
+      newChoices[e.target.value].points += 1;
+      e.target.classList = [];
+    } else {
+      newChoices[e.target.value].points -= 1;
+      e.target.classList = ['inactive'];
+    }
+    this.setState({
+      choices: newChoices
+    });
   }
 
   render() {
@@ -47,8 +58,8 @@ class TopicVote extends React.Component {
           {this.state.choices.map((choice, index) => (
             <li key={index.toString()}>
               <span>{choice.choice}</span>
-              <span>  Points: {choice.points}</span>
-              <button value={index.toString()} type="button" onClick={this.handleUpvote}> + </button>
+              <span>  {choice.points} pts  </span>
+              <button value={index.toString()} type="button" onClick={this.handleUpvote} className="inactive"> vote </button>
             </li>
           ))}
         </ul>
